3516e855f42c7fb22df93e5c340758602c1d336b
Pratik-Singh-Deo <pratiksinghdeo@gmail.com>
2024-10-23 08:51:43 +0530

    redux toolkit, fexed import errors

--- README.md
+++ README.md
@@ -10,4 +10,21 @@
 - Create BrowserRouter > Routes > Route=/Body > RouteChildren
 - Create an Outlet in your Body Component
 - Create a footer
-- Create a Login Page.
+- Create a Login Page
+- Install axios
+- CORS - install cors in backend => add middleware to with configurations: origin, credentials: true
+- Whenever you're making API call so pass axios => {withCredentials: true}
+- Install react-redux + @reduxjs/toolkit - https://redux-toolkit.js.org/tutorials
+- configureStore => Provider => creteSlice => add reducer to store
+- Add redux devtools in chrome
+- Login and see if your data is coming properly in the store
+- NavBar should update as soon as user logs in
+- Refactor our code to add constants file + create a components folder
+
+# Body
+
+Navbar
+Route=/ => Feed
+Route=/login => Login
+Route=/connections => Connections
+Router=/profile => Profile
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+
   return (
-    <div>
-      <div className="navbar bg-neutral text-neutral-content">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">🧑🏻‍💻DevTinder</a>
-        </div>
+    <div className="navbar bg-neutral text-neutral-content">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">🧑🏻‍💻DevTinder</a>
+      </div>
+      {user && (
         <div className="flex-none gap-2">
-          <div className="form-control"></div>
-          <div className="dropdown dropdown-end mx-3">
+          <div className="form-control">Welcome, {user.firstName}</div>
+          <div className="dropdown dropdown-end mx-3 flex ">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -41,7 +42,7 @@ const NavBar = () => {
             </ul>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
